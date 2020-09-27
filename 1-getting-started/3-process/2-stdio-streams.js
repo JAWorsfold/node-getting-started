9286ed13a7db3251dac1c42fc5350d2d9f5476c4
Joe Worsfold <joseph.worsfold@digital.homeoffice.gov.uk>
2020-09-27 17:34:14 +0100

    Recorded use of pipe for process stdin and stdout

--- 1-getting-started/3-process/2-stdio-streams.js
+++ 1-getting-started/3-process/2-stdio-streams.js
@@ -1,6 +1,9 @@
-process.stdin.on('readable', () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(chunk);
-  }
-});
+// process.stdin.on('readable', () => {
+//   const chunk = process.stdin.read();
+//   if (chunk !== null) {
+//     process.stdout.write(chunk);
+//   }
+// });
+
+// same as above in a single line
+process.stdin.pipe(process.stdout);
