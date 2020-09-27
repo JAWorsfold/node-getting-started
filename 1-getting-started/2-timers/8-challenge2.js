bbfd6005f5c33da96def795f646777bdd0abc553
Joe Worsfold <joseph.worsfold@digital.homeoffice.gov.uk>
2020-09-27 15:54:44 +0100

    Completed interval challenge

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -1,5 +1,25 @@
 // Print "Hello World"
 // Every second
 // And stop after 5 times
-
 // After 5 times. Print "Done" and let Node exit.
+
+let counter = 1;
+
+const func = () => {
+  console.log("Hello World")
+  if (counter === 5) {
+    clearInterval(interval);
+    console.log("Done");
+  }
+  counter += 1;
+}
+
+const interval = setInterval(func, 1000);
+
+// can't use setTimeout
+// const func = inter => {
+//   clearInterval(inter);
+//   console.log("Done");
+// };
+
+// setTimeout(func, 5500, interval);
