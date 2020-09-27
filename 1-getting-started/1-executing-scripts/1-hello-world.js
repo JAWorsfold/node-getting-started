d2daba5f714cc0f77f12f3d5282a00cb26cf72d0
Joe Worsfold <joeworsfold@hod083485.local>
2020-09-27 14:14:46 +0100

    Got started

--- 1-getting-started/1-executing-scripts/1-hello-world.js
+++ 1-getting-started/1-executing-scripts/1-hello-world.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.end('Hello World\n');
+  res.end('Welcome to the world of tomorrow...\n');
 });
 
 server.listen(4242, () => {
